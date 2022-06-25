6939f97b9855bb63883dcf018ee9ef20a0024170
Aredli <xtarak@gmail.com>
2022-06-25 23:04:27 +0200

    Refactor posts.service.ts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -6,7 +6,7 @@ import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
-  async create(createPostDto: CreatePostDto) {
+  async create(createPostDto: CreatePostDto): Promise<object> {
     try {
       const response = await getFirestore()
         .collection('posts')
@@ -33,36 +33,28 @@ export class PostsService {
     return await this.getAllPost();
   }
 
-  async findAllByType(type: string) {
-    try {
-      const posts: Post[] = [];
-      (
-        await getFirestore().collection('posts').where('type', '==', type).get()
-      ).docs.map((data) => {
-        posts.push({ id: data.id, ...(data.data() as Post) });
-      });
-      return posts;
-    } catch (error) {
-      return { error: error };
-    }
+  async findAllByType(type: string): Promise<Post[]> {
+    const posts: Post[] = [];
+    (
+      await getFirestore().collection('posts').where('type', '==', type).get()
+    ).docs.map((data) => {
+      posts.push({ id: data.id, ...(data.data() as Post) });
+    });
+    return posts;
   }
 
-  async findImportantByType(type, count) {
-    try {
-      const posts: Post[] = [];
-      (
-        await getFirestore()
-          .collection('posts')
-          .where('type', '==', type)
-          .where('important', '==', true)
-          .get()
-      ).docs.map((data) => {
-        posts.push({ id: data.id, ...(data.data() as Post) });
-      });
-      return posts;
-    } catch (error) {
-      return { error: error };
-    }
+  async findImportantByType(type, count): Promise<Post[]> {
+    const posts: Post[] = [];
+    (
+      await getFirestore()
+        .collection('posts')
+        .where('type', '==', type)
+        .where('important', '==', true)
+        .get()
+    ).docs.map((data) => {
+      posts.push({ id: data.id, ...(data.data() as Post) });
+    });
+    return posts;
   }
 
   async findOne(id: string): Promise<Post> {
@@ -70,7 +62,7 @@ export class PostsService {
     return { id: response.id, ...(response.data() as Post) };
   }
 
-  async findPopular(count: number) {
+  async findPopular(count: number): Promise<Post[]> {
     let posts: Post[] = await this.getAllPost();
     posts = posts.filter((post) => post.likes); // Delete not liked posts
     posts = posts.sort((a, b) => b.likes - a.likes); // Sorting from most liked to least liked
@@ -78,7 +70,7 @@ export class PostsService {
     return posts;
   }
 
-  async findMostViewed(count: number) {
+  async findMostViewed(count: number): Promise<Post[]> {
     let posts: Post[] = await this.getAllPost();
     posts = posts.filter((post) => post.views); // Delete not viewed posts
     posts = posts.sort((a, b) => b.views - a.views); // Sorting from most viewed to least viewed
@@ -86,7 +78,7 @@ export class PostsService {
     return posts;
   }
 
-  async update(id: string, updatePostDto: UpdatePostDto) {
+  async update(id: string, updatePostDto: UpdatePostDto): Promise<object> {
     try {
       await getFirestore()
         .collection('posts')
